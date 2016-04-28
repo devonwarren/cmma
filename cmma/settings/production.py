8935cad84fa8b4bb5848b55ea8957622756a7a37
Devon Warren <devon.warren@gmail.com>
2016-04-28 12:06:51 -0400

    add postgres to production settings

--- cmma/settings/production.py
+++ cmma/settings/production.py
@@ -5,3 +5,12 @@ DEBUG = False
 ALLOWED_HOSTS = ['107.170.47.238', 'cmmatsd.com', ]
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	'NAME': 'cmma',
+	'USER': 'cmma',
+	'HOST': 'localhost',
+	'PORT': '5432',
+    }
+}
